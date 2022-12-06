ed606e211e50a9575a8145774a7ee2ab4e514363
Trent <ghastfilms613@gmail.com>
2022-12-06 16:50:06 -0600

    removed sets from day 3 solution

--- src/Day3.hs
+++ src/Day3.hs
@@ -4,25 +4,23 @@ import Util
 import Solver
 
 import Data.Char
-import Data.Set (Set)
-import qualified Data.Set as Set
 
 day3 :: Solver
 day3 = mkSolver 3 "Rucksack Reorganization" $ (\s -> 
     let cont = lines s
-        f = sum . fmap (priorityOf . head . Set.toList)
-        a = f $ uncurry Set.intersection . compartments <$> cont
-        b = f $ groupIntersection . fmap Set.fromList <$> groupsOf 3 cont    
+        f = sum . fmap (priorityOf . head)
+        a = f $ uncurry union . compartments <$> cont
+        b = f $ groupIntersection <$> groupsOf 3 cont    
     in (a,b))
 
-compartments :: Ord a => [a] -> (Set a, Set a)
-compartments r = (Set.fromList $ take n r, Set.fromList $ drop n r)
+compartments :: [a] -> ([a], [a])
+compartments r = (take n r, drop n r)
     where n = length r `div` 2
 
 priorityOf :: Char -> Int
 priorityOf c = ord c - if isUpper c then 38 else 96
 
-groupIntersection :: Ord a => [Set a] -> Set a
-groupIntersection [] = Set.empty
+groupIntersection :: Ord a => [[a]] -> [a]
+groupIntersection [] = []
 groupIntersection [x] = x
-groupIntersection (x:xs) = x `Set.intersection` groupIntersection (xs)
+groupIntersection (x:xs) = x `union` groupIntersection xs
