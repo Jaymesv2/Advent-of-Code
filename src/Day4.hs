f4d12f7cd0ba51e38387c58638825ab308237dc2
Trent <ghastfilms613@gmail.com>
2022-12-06 11:08:06 -0600

    updated day 4

--- src/Day4.hs
+++ src/Day4.hs
@@ -9,10 +9,11 @@ day4 = do
     pure (calcAnsw completeOverlap,calcAnsw anyOverlap)
 
 completeOverlap :: ((Int, Int), (Int,Int)) -> Bool
+--completeOverlap ((a,b), (x,y)) = ((x < a && y > b) || (a < x && b > y))
 completeOverlap ((a,b), (x,y)) = ((a <= x && b >= y) || (x <= a && y >= b))
 
 anyOverlap :: ((Int, Int), (Int,Int)) -> Bool
-anyOverlap ((a,b), (x,y)) = (b >= x) && (y >= a)
+anyOverlap ((a,b), (x,y)) = (x < b) && (a < y)
 
 parseInput :: String -> ((Int, Int), (Int,Int))
 parseInput = (f *** f) . second tail . break (==',') 
