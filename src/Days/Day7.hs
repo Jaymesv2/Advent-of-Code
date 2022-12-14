ed06f1d9eed278071d711107bbf113e7d315e35d
Trent <ghastfilms613@gmail.com>
2022-12-14 16:26:53 -0600

    day 7

--- src/Days/Day7.hs
+++ src/Days/Day7.hs
@@ -1,56 +1,36 @@
+{-# LANGUAGE TupleSections #-}
 module Days.Day7(day7) where
 
 import Solver
---import Util
-
-import Debug.Trace
-
-import Data.List(foldl', intercalate)
-import Data.Char
-import Data.Maybe
 
 import Control.Applicative ((<|>))
-import Control.Monad.Trans.Writer.Lazy
 import Control.Arrow
-
+import Data.Char
+import Data.Maybe
+import Data.List (foldl')
+import Data.Map.Lazy (Map, empty, toList, alter)
 import Text.ParserCombinators.ReadP
 
-data Token = Tfile (Int)
-           | Tcd
-           | TcdDot
-           deriving (Show, Eq)
+data Token = Tfile Int | Tcd String | TcdDot deriving (Show)
 
 tokenize :: String -> [Token]
-tokenize = mapMaybe (fmap fst . listToMaybe . reverse . readP_to_S (cd <|> file)) . lines
-    where cd = string "$ cd " *> (TcdDot <$ string "..") <++ (Tcd <$ many1 (satisfy isPrint))
+tokenize = lines >>> mapMaybe (readP_to_S (cd <|> file) >>> reverse >>> listToMaybe >>> fmap fst)
+    where cd = string "$ cd " *> (TcdDot <$ string "..") <++ (Tcd <$> many1 (satisfy isPrint))
           file = fmap Tfile $ (read) <$> (many1 (satisfy isDigit) <* char ' ') <* (many1 (satisfy isPrint))
 
+makeMap ::  Map [String] Int -> [Token] -> Map [String] Int
+makeMap h = foldl' step ([], h) >>> snd where 
+        step :: ([String], Map [String] Int) -> Token -> ([String], Map [String] Int)
+        step (path, sizes) (Tcd s) = (s:path, sizes)
+        step (_:ps, sizes) (TcdDot) = (ps, sizes)
+        step (path, sizes) (Tfile size) = (path, updateMap path size sizes)
+        step ([], _) TcdDot = error "cant move past root directory"
+
+        updateMap :: [String] -> Int -> Map [String] Int -> Map [String] Int
+        updateMap [] _ sizes = sizes
+        updateMap x@(_:xs) size sizes = updateMap xs size (alter (\s -> case s of 
+            (Just i) -> Just (size+i)
+            Nothing -> Just size) x sizes)
+
 day7 :: Solver
-day7 = mkSolver 7 "No Space Left On Device" $ \inp -> 
-    let tokens = tokenize inp
-        y = finishEval . foldl' evalToken ([],[]) $ tokens
-    in ("incomplete", "incomplete")
-
-data File a = Dir [File a] 
-          | File a
-          deriving (Eq, Show)
-
-type State = ([File Int], [File Int])
-
-evalToken :: State -> Token -> State
-evalToken (x,    fs) Tcd           = (Dir fs:x, [])
-evalToken ((Dir ds):xs, fs) TcdDot = (xs, (Dir fs):ds)
-evalToken (_,    s) TcdDot         = error ""
-evalToken (x,    ds) (Tfile s)     = (x, File s:ds)
-
-finishEval :: State -> File Int
-finishEval ((Dir ds):xs, fs) = finishEval (xs, (Dir fs):ds)
-finishEval ([], fs) = Dir fs
-
-{-
-To begin, find all of the directories with a total size of at most 100000, 
-then calculate the sum of their total sizes.
-(As in this example, this process can count files more than once!)
--}
-
-
+day7 = mkSolver 7 "No Space Left On Device" $ tokenize >>> makeMap empty >>> toList >>> fmap snd >>> ((filter (<= 100000) >>> sum) &&& ((\f -> filter (>  maximum f-40000000) f) >>> minimum))
