0845cd6fd8f891718f13ec9c91830880234c29da
Trent <ghastfilms613@gmail.com>
2022-12-14 16:28:14 -0600

    updated day 12

--- src/Days/Day12.hs
+++ src/Days/Day12.hs
@@ -1,84 +1,35 @@
-{-# LANGUAGE TupleSections #-}
 module Days.Day12(day12) where
 
 import Solver
 
-import Data.Graph
 import Data.Char
 
 import Control.Arrow
-import Debug.Trace
 import Data.Maybe
-
+import Data.List (findIndices, repeat)
 import Algorithm.Search
 
-{-
-import Data.GraphViz
-import Data.GraphViz.Commands
-import System.IO.Unsafe
-import qualified Data.Graph.Inductive.Graph as G
-import qualified Data.Graph.Inductive.PatriciaTree as P
-
-runMe :: IO String
-runMe = do
-    input <- readFile "inputs/12.txt"
-    let (graph, vert, key) = makeGraph $ parseInput input
-        unwrap s = case s of
-            Just n -> n
-            Nothing -> error "failed to unwrap"
-        x :: Vertex -> G.Context Char Int
-        x v = ([], v, l, ((1,) . (unwrap . key)) <$> n) where (l, _, n) = vert v
-        fglGraph :: P.Gr Char Int
-        fglGraph = G.buildGr (x <$> vertices graph)
-        gv = (setDirectedness graphToDot params) $  fglGraph
-        params = quickParams
-    runGraphvizCanvas' gv Xlib
-    pure "hi"
--}
-
-solve :: (GraphG -> [Key]) -> GraphG -> Int
-solve getPos g@(_,vert,key) = minimum (mapMaybe getDist $ getPos g)
-    where 
-          getDist start = length . snd <$> dijkstra getNeighbors (\_ _ -> 1) ((==) end) start
-          end = head $ allXPos 'E' g
-          getNeighbors s = let (_,_,n) = unwrap $ vert <$> key s in n
-          unwrap s = case s of
-            Just n -> n
-            Nothing -> error "failed to unwrap"
-
-solve :: (GraphG -> [Key]) -> GraphG -> Int
-solve getPos g@(_,vert,key) = minimum (mapMaybe getDist $ getPos g)
-    where 
-          getDist start = length . snd <$> dijkstra getNeighbors (\_ _ -> 1) ((==) end) start
-          end = head $ allXPos 'E' g
-          getNeighbors s = let (_,_,n) = unwrap $ vert <$> key s in n
-          unwrap s = case s of
-            Just n -> n
-            Nothing -> error "failed to unwrap"
-
--- a list of rows
 type Matrix a = [[a]]
 type Key = (Int, Int)
-type GraphG = (Graph, Vertex -> (Char, Key, [Key]), Key -> Maybe Vertex)
 
 day12 :: Solver
-day12 = mkSolver 12 "Hill Climbing Algorithm" $ lines >>> makeGraph >>> (solve (allXPos 'S') &&& solve (allXPos 'a'))
+day12 = mkSolver 12 "Hill Climbing Algorithm" $ lines >>> (part1 &&& part2)
 
-allXPos :: Char -> GraphG -> [Key]
-allXPos x (g,vert, _) = mapMaybe filterPos (vertices g)
-    where filterPos v = if c == x then Just k else Nothing where (c,k,_) = vert v 
+part1, part2 :: Matrix Char -> Int
+part1 = solve (allXPos 'S' >>> head) (allXPos 'E') (\x y -> x - y >= -1)
+part2 = solve (allXPos 'E' >>> head) (allXPos 'a') (\x y -> y - x >= -1 )
 
-makeGraph :: Matrix Char -> GraphG
-makeGraph matrix = graphFromEdges [go (m,n) | m <- [0..lm-1], n <- [0..ln-1]]
-    where
-        lm = length matrix
-        ln = length (head matrix)
-        mAt :: Key -> Int
-        mAt (m,n) = case (matrix !! m) !! n of 
-                'S' -> 0 -- S
-                'E' -> 26  -- E
-                c   -> ord c - 97
-        go :: Key -> (Char, Key, [Key])
-        go (m,n) = ((matrix !! m) !! n, (m,n), neighbors)
-            where neighbors = filter (\(x, y) -> (x >= 0) && (x < lm) && (y >= 0) && (y < ln) && (mAt (m,n) - mAt (x,y) >= -1))
-                    [(m+1, n), (m-1, n), (m, n+1), (m, n-1)]
+allXPos :: Char -> Matrix Char -> [Key]
+allXPos x matrix = concat $ zipWith (\y l -> zipWith (,) (repeat y) l) [0..] $ fmap (findIndices (==x)) matrix
+
+solve :: (Matrix Char -> Key) -> (Matrix Char -> [Key]) -> (Int -> Int -> Bool) -> Matrix Char -> Int
+solve startF endF canMove matrix = length $ maybe [] snd $ dijkstra getNeighbors (\_ _ -> 1) (flip elem $ endF matrix) $ startF matrix
+    where lm = length matrix
+          ln = length (head matrix)
+          getNeighbors :: Key -> [Key]
+          getNeighbors (m,n) = filter (\(x, y) -> x >= 0 && x < lm && y >= 0 && y < ln && canMove (mAt (m,n)) (mAt (x,y))) [(m+1, n), (m-1, n), (m, n+1), (m, n-1)]
+          mAt :: Key -> Int
+          mAt (m,n) = case (matrix !! m) !! n of 
+            'S' -> 0
+            'E' -> 26
+            c   -> ord c - 97
