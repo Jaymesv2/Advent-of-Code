baa7cdf2f39df2283e5c8db4a4ac8fffaba12dcb
Trent <ghastfilms613@gmail.com>
2022-12-18 17:11:38 -0600

    updated day 15

--- src/Days/Day15.hs
+++ src/Days/Day15.hs
@@ -7,39 +7,21 @@ import Control.Arrow
 import Control.Applicative
 import Text.ParserCombinators.ReadP
 import Data.Char
-import Data.List (sortBy, foldl', intercalate)
-import Debug.Trace
-import qualified Data.IntSet as S
+import Data.List (sortBy, foldl')
 
 type Cordinate = (Int, Int)
 
-sampleInput :: String
-sampleInput = "Sensor at x=2, y=18: closest beacon is at x=-2, y=15\nSensor at x=9, y=16: closest beacon is at x=10, y=16\nSensor at x=13, y=2: closest beacon is at x=15, y=3\nSensor at x=12, y=14: closest beacon is at x=10, y=16\nSensor at x=10, y=20: closest beacon is at x=10, y=16\nSensor at x=14, y=17: closest beacon is at x=10, y=16\nSensor at x=8, y=7: closest beacon is at x=2, y=10\nSensor at x=2, y=0: closest beacon is at x=2, y=10\nSensor at x=0, y=11: closest beacon is at x=2, y=10\nSensor at x=20, y=14: closest beacon is at x=25, y=17\nSensor at x=17, y=20: closest beacon is at x=21, y=22\nSensor at x=16, y=7: closest beacon is at x=15, y=3\nSensor at x=14, y=3: closest beacon is at x=15, y=3\nSensor at x=20, y=1: closest beacon is at x=15, y=3"
-
 day15 :: Solver
-day15 = mkSolver 15 "Beacon Exclusion Zone" $ 
-    const sampleInput >>> 
-    parseInput >>> (part1 &&& const 0)
-
-traceS x = traceShow x x
+day15 = mkSolver 15 "Beacon Exclusion Zone" $ parseInput >>> (part1 &&& const "unimplemented")
 
 part1 :: [(Cordinate, Cordinate)] -> Int
-part1 beacons = --traceShow (beaconsOnLine) $ mx-mi
-            S.size nums
+part1 beacons = length (filter (\x -> any (\(c, b) -> inRangeOf c b (x,y)) beaconDists) [mi..mx])-1
     where 
-        --y = 2000000
-        y = 10
-        --uuu :: [(Cordinate, Int)] -> [Int]
-        --uuu bDists = filter (\x -> any (\(c, b) -> inRangeOf c b (x,y)) bDists) $ filter (\x -> ((x,y) `notElem` fmap fst bDists) && (x,y) `notElem` fmap snd beacons) $ makeLine y bDists
-
+        y = 2000000
         beaconDists :: [(Cordinate, Int)]
         beaconDists = makeDistChecker <$> sortBy (\((x,_),_) ((y2,_),_) -> compare x y2) beacons
-
-        beaconsOnLine = length $ filter (\((x', y'),_) -> y'==y) beaconDists
-        
-        (mi,mx,nums) = foldl' (\(a1, b1, set) (a2, b2) -> (min a1 a2, max b1 b2, S.union set $ S.fromAscList [a2..b2-1])) (maxBound,minBound, S.empty) $ traceS intersections
-
-        intersections = intersectionsOnLine y <$> beaconDists
+        (mi,mx) = foldl' (\(a1, b1) (a2, b2) -> (min a1 a2, max b1 b2)) (maxBound,minBound) intersections
+        intersections = sortBy (\(a,_) (b,_)-> compare a b) $ intersectionsOnLine y <$> beaconDists
 
 parseInput :: String -> [(Cordinate, Cordinate)]
 parseInput = readP_to_S (sepBy1 beaconP $ string "\n") >>> last >>> fst
@@ -47,23 +29,26 @@ parseInput = readP_to_S (sepBy1 beaconP $ string "\n") >>> last >>> fst
           beaconP = liftA2 (,) (string "Sensor at " *> xyP) (string ": closest beacon is at " *> xyP)
           numP = read <$> liftA2 (++) (option "" $ string "-") (many1 $ satisfy isDigit)
 
-intersectionsOnLine :: Int -> (Cordinate, Int) -> (Int, Int)
+intersectionsOnLine :: Int -> (Cordinate, Int) -> Cordinate
 intersectionsOnLine y ((bx, by), dist) = if a < b then (a,b) else (b,a) 
     where (a, b) = ((bx-) &&& (bx+)) $ dist - abs (by - y)
-
 
 distance :: Cordinate -> Cordinate -> Int
 distance (x1,y1) (x2,y2) = abs (x1-x2) + abs (y1-y2)
 
 makeDistChecker :: (Cordinate, Cordinate) -> (Cordinate, Int)
 makeDistChecker (beacon, sensor) = (beacon, distance beacon sensor)
-{-
-makeLine :: Int -> [(Cordinate, Int)] -> [Int]
-makeLine y xs = [mi..mx]
-    where (mi,mx) =(\x -> traceShow x x) $ foldl' (\(a1, b1) (a2, b2) -> (min a1 a2, max b1 b2)) (maxBound,0) (intersectionsOnLine y <$> xs)
 
 inRangeOf :: Cordinate -> Int -> Cordinate -> Bool
 inRangeOf base radius other = distance base other <= radius
+
+{-
+sampleInput :: String
+sampleInput = "Sensor at x=2, y=18: closest beacon is at x=-2, y=15\nSensor at x=9, y=16: closest beacon is at x=10, y=16\nSensor at x=13, y=2: closest beacon is at x=15, y=3\nSensor at x=12, y=14: closest beacon is at x=10, y=16\nSensor at x=10, y=20: closest beacon is at x=10, y=16\nSensor at x=14, y=17: closest beacon is at x=10, y=16\nSensor at x=8, y=7: closest beacon is at x=2, y=10\nSensor at x=2, y=0: closest beacon is at x=2, y=10\nSensor at x=0, y=11: closest beacon is at x=2, y=10\nSensor at x=20, y=14: closest beacon is at x=25, y=17\nSensor at x=17, y=20: closest beacon is at x=21, y=22\nSensor at x=16, y=7: closest beacon is at x=15, y=3\nSensor at x=14, y=3: closest beacon is at x=15, y=3\nSensor at x=20, y=1: closest beacon is at x=15, y=3"
+
+makeLine :: Int -> [(Cordinate, Int)] -> [Int]
+makeLine y xs = [mi..mx]
+    where (mi,mx) =(\x -> traceShow x x) $ foldl' (\(a1, b1) (a2, b2) -> (min a1 a2, max b1 b2)) (maxBound,0) (intersectionsOnLine y <$> xs)
 
 inRangeOfAny' :: [(Cordinate, Int)] -> Cordinate -> Char
 inRangeOfAny' beacons cord
